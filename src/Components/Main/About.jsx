83165f7891bf5ed5bcae2bd62beb0b2813b92cf9
John Brooks <johnrbrooks93@gmail.com>
2023-08-17 19:14:46 -0400

    About Page built out

--- src/Components/Main/About.jsx
+++ src/Components/Main/About.jsx
@@ -21,6 +21,60 @@ export default function About () {
                         if you had an A Day To Remember CD in your car and get the reference. ;-)
                         </p>
                     </div>
+                    <div className="members-grid">
+                        <div className="member-section">
+                            <img src="src/assets/images/TylerGoofy.jpg" alt="" className="member-image" />
+                            <div className="member-content">
+                                <h2 className="member-name">Tyler Schafer</h2>
+                                <h3 className="member-role">Lead Vocals, Guitar, Air/Chest Drums</h3>
+                                <hr />
+                                <p className="member-about">
+                                Tyler’s a JMU grad, percussionist, audio engineer, mix engineer, producer, and aspiring
+                                Kermit the Frog impersonator. He went so hard during Black Parade that he chipped a tooth
+                                on the mic and he sweats from his knees. It was never a phase.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="member-section">
+                            <img src="src/assets/images/JohnGoofy.jpg" alt="" className="member-image" />
+                            <div className="member-content">
+                                <h2 className="member-name">John Brooks</h2>
+                                <h3 className="member-role">Lead Guitar, Setlists, Windowsill Whiskey Sours</h3>
+                                <hr />
+                                <p className="member-about">
+                                John’s a JMU grad, audio engineer, mix engineer, producer, hockey player, and learning to
+                                code like every other millennial dude. He specializes in nailing solos at home and then getting
+                                flustered on stage when Tyler says his name the measure before.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="member-section">
+                            <img src="src/assets/images/JasonGoofy.jpg" alt="" className="member-image" />
+                            <div className="member-content">
+                                <h2 className="member-name">Jason Eller</h2>
+                                <h3 className="member-role">Bass, Backing Vocals, IEM Rig</h3>
+                                <hr />
+                                <p className="member-about">
+                                Jason’s a VT grad, a normal 9-5 kind of engineer, and just bought a house (what a sell out).
+                                He’s been playing bass and nailing the echos on Stacy’s Mom since high school, but he’s still
+                                got a deer-in-headlights look on stage. Buy him whatever pilsner’s on tap so he can chill a bit.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="member-section">
+                            <img src="src/assets/images/JamesGoofy.jpg" alt="" className="member-image" />
+                            <div className="member-content">
+                                <h2 className="member-name">James Rocket (Adelsberger)</h2>
+                                <h3 className="member-role">Drums, Band Morale, Blisters</h3>
+                                <hr />
+                                <p className="member-about">
+                                James is working on his OT certification when he’s not playing drums or guitar with one of his
+                                273 side projects (only a mild exaggeration). He was the mastermind behind the half-time
+                                breakdown in The Middle. You’re welcome for the spiciness.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <Footer />
