8672b9766fa139798f5b79dea64da5ebbb0da243
John Brooks <johnrbrooks93@gmail.com>
2023-08-27 17:06:56 -0400

    Shows page started

--- src/Components/Main/Shows.jsx
+++ src/Components/Main/Shows.jsx
@@ -9,6 +9,22 @@ export default function Shows () {
             <div className="home-page">
                 <Nav />
                 <h1>Shows</h1>
+                <div className="shows-grid">
+                    <div className="show-item">
+                        <h2 className="venue-name"></h2>
+                        <img src="" alt="" className="show-poster" />
+                        <div className="show-data-grid">
+                            <h3 className="show-data-title">Date:</h3>
+                            <p className="show-data-info"></p>
+                            <h3 className="show-data-title">Time:</h3>
+                            <p className="show-data-info"></p>
+                            <h3 className="show-data-title">Location:</h3>
+                            <p className="show-data-info"></p>
+                            <h3 className="show-data-title">Cover?</h3>
+                            <p className="show-data-info"></p>
+                        </div>
+                    </div>
+                </div>
                 <Footer />
             </div>
         </div>
