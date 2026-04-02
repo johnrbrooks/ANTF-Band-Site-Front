c601ed4d081ad4ffdb79a55d50ebc87e94ce30af
John Brooks <johnrbrooks93@gmail.com>
2026-04-02 19:17:21 -0400

    Added light horse imgur link to helper text

--- src/Components/Admin/AdminForms/AddShowForm.jsx
+++ src/Components/Admin/AdminForms/AddShowForm.jsx
@@ -1,107 +1,176 @@
-import './AdminHome.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { BASE_URL } from '../../../App'
+import "./AdminHome.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../../App";
 
-export default function AddShowForm () {
-
+export default function AddShowForm() {
     const initialState = {
-        venue: '',
-        show_poster: '',
-        location: '',
-        date: '',
-        time: '',
-        cover: '',
-    }
+        venue: "",
+        show_poster: "",
+        location: "",
+        date: "",
+        time: "",
+        cover: "",
+    };
 
-    const [formData, setFormData] = useState(initialState)
-    const [successMessage, setSuccessMessage] = useState('')
-    const [formError, setFormError] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
+    const [formData, setFormData] = useState(initialState);
+    const [successMessage, setSuccessMessage] = useState("");
+    const [formError, setFormError] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleChange = (e) => {
-        setErrorMessage('')
-        setSuccessMessage('')
-        setFormError('')
-        const { name, value } = e.target
+        setErrorMessage("");
+        setSuccessMessage("");
+        setFormError("");
+        const { name, value } = e.target;
         setFormData((prevFormData) => ({
             ...prevFormData,
             [name]: value,
-        }))
-    }
+        }));
+    };
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        if(!Object.values(formData).every(value => value !== '')) {
-            setFormError('You have not filled in all the fields.')
-            return
+        if (!Object.values(formData).every((value) => value !== "")) {
+            setFormError("You have not filled in all the fields.");
+            return;
         }
-        if(!formData.show_poster.includes('i.imgur')) {
-            setFormError('Your poster link will not work.')
-            return
-        } 
-        if(!isValidDate(formData.date)) {
-            setFormError('Your date is not correctly formatted.')
-            return
+        if (!formData.show_poster.includes("i.imgur")) {
+            setFormError("Your poster link will not work.");
+            return;
         }
-        addShow()
-    }
+        if (!isValidDate(formData.date)) {
+            setFormError("Your date is not correctly formatted.");
+            return;
+        }
+        addShow();
+    };
 
     const isValidDate = (input) => {
-        const regex = /^\d{4}-\d{2}-\d{2}$/
-        return regex.test(input)
-    }
+        const regex = /^\d{4}-\d{2}-\d{2}$/;
+        return regex.test(input);
+    };
 
     const addShow = async () => {
         try {
-            const response = await axios.post(`${BASE_URL}shows/create`, formData)
-            if(response.status === 200) {
-                setSuccessMessage('The show has been added!')
+            const response = await axios.post(
+                `${BASE_URL}shows/create`,
+                formData,
+            );
+            if (response.status === 200) {
+                setSuccessMessage("The show has been added!");
             } else {
-                setErrorMessage('There was an error creating the show.')
+                setErrorMessage("There was an error creating the show.");
             }
         } catch (error) {
-            console.error('There was an error creating the show: ', error)
+            console.error("There was an error creating the show: ", error);
         }
-    }
+    };
 
     return (
         <>
             <h1>Add Show Form</h1>
             <form className="show-form" action="" onSubmit={handleSubmit}>
-                <p className="instructions">Please follow the formatting exactly when submitting the data.</p>
+                <p className="instructions">
+                    Please follow the formatting exactly when submitting the
+                    data.
+                </p>
                 <ol className="image-instructions">
-                    <li className="instructions">To create an image link, go to <a href="https://imgur.com/"> Imgur.com </a>and sign in using Google and the band's email.</li>
-                    <li className="instructions">Then go to Account/Images and click Add Images.</li>
-                    <li className="instructions">Upload the poster image and save.</li>
-                    <li className="instructions">Then click on the saved image, right click the image and copy the image address.</li>
-                    <li className="instructions">DO NOT use the Image link displayed near the image.</li>
-                    <li className="instructions">It should match the EXACT format of the link shown below.</li>
+                    <li className="instructions">
+                        To create an image link, go to{" "}
+                        <a href="https://imgur.com/"> Imgur.com </a>and sign in
+                        using Google and the band's email.
+                    </li>
+                    <li className="instructions">
+                        Then go to Account/Images and click Add Images.
+                    </li>
+                    <li className="instructions">
+                        Upload the poster image and save.
+                    </li>
+                    <li className="instructions">
+                        Then click on the saved image, right click the image and
+                        copy the image address.
+                    </li>
+                    <li className="instructions">
+                        DO NOT use the Image link displayed near the image.
+                    </li>
+                    <li className="instructions">
+                        It should match the EXACT format of the link shown
+                        below.
+                    </li>
                 </ol>
                 <label htmlFor="">Venue:</label>
                 <p className="format-example">Format: The Light Horse</p>
-                <input type="text" name="venue" value={formData.venue} className="data-input" placeholder="Venue Name" onChange={handleChange}/>
+                <input
+                    type="text"
+                    name="venue"
+                    value={formData.venue}
+                    className="data-input"
+                    placeholder="Venue Name"
+                    onChange={handleChange}
+                />
                 <label htmlFor="">Show Poster:</label>
-                <p className="format-example">Format: https://i.imgur.com/T4lMfpo.png</p>
-                <input type="text" name="show_poster" value={formData.show_poster} className="data-input" placeholder="Poster URL" onChange={handleChange}/>
+                <p className="format-example">
+                    Format: https://i.imgur.com/mPI5Kkl.png
+                </p>
+                <input
+                    type="text"
+                    name="show_poster"
+                    value={formData.show_poster}
+                    className="data-input"
+                    placeholder="Poster URL"
+                    onChange={handleChange}
+                />
                 <label htmlFor="">Location:</label>
                 <p className="format-example">Format: Alexandria, VA</p>
-                <input type="text" name="location" value={formData.location} className="data-input" placeholder="Location" onChange={handleChange}/>
+                <input
+                    type="text"
+                    name="location"
+                    value={formData.location}
+                    className="data-input"
+                    placeholder="Location"
+                    onChange={handleChange}
+                />
                 <label htmlFor="">Date:</label>
                 <p className="format-example">Format: 2023-09-30</p>
-                <input type="text" name="date" value={formData.date} className="data-input" placeholder="Date" onChange={handleChange}/>
+                <input
+                    type="text"
+                    name="date"
+                    value={formData.date}
+                    className="data-input"
+                    placeholder="Date"
+                    onChange={handleChange}
+                />
                 <label htmlFor="">Time:</label>
                 <p className="format-example">Format: 9:30pm - 1:00am</p>
-                <input type="text" name="time" value={formData.time} className="data-input" placeholder="Time" onChange={handleChange}/>
+                <input
+                    type="text"
+                    name="time"
+                    value={formData.time}
+                    className="data-input"
+                    placeholder="Time"
+                    onChange={handleChange}
+                />
                 <label htmlFor="">Cover:</label>
                 <p className="format-example">Format: Free || $5</p>
-                <input type="text" name="cover" value={formData.cover} className="data-input" placeholder="Cover" onChange={handleChange}/>
-                {formError && <p className='error-message'>{formError}</p>}
-                {errorMessage && <p className="submission-error">{errorMessage}</p>}
+                <input
+                    type="text"
+                    name="cover"
+                    value={formData.cover}
+                    className="data-input"
+                    placeholder="Cover"
+                    onChange={handleChange}
+                />
+                {formError && <p className="error-message">{formError}</p>}
+                {errorMessage && (
+                    <p className="submission-error">{errorMessage}</p>
+                )}
                 <button className="song-form-submit">Add</button>
-                {successMessage && <p className="success-message">{successMessage}</p>}
+                {successMessage && (
+                    <p className="success-message">{successMessage}</p>
+                )}
             </form>
         </>
-    )
+    );
 }
--- src/Components/Main/HomePage/NextShow.jsx
+++ src/Components/Main/HomePage/NextShow.jsx
@@ -1,79 +1,86 @@
-import { useState, useEffect, useRef } from 'react'
-import { BASE_URL } from '../../../App'
-import axios from 'axios'
-import moment from 'moment'
-import { Link } from 'react-router-dom'
+import { useState, useEffect, useRef } from "react";
+import { BASE_URL } from "../../../App";
+import axios from "axios";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
-export default function NextShow () {
+export default function NextShow() {
+    const [shows, setShows] = useState([]);
+    const [nextShow, setNextShow] = useState();
 
-    const [shows, setShows] = useState([])
-    const [nextShow, setNextShow] = useState()
-
-    const elementRef = useRef(null)
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('visible')
-                } else {
-                    entry.target.classList.remove('visible')
-                }
-            })
-        }, { threshold: 0.1 })
-
-            if(elementRef.current) {
-                observer.observe(elementRef.current)
-            }
-
-            return () => {
-                if (elementRef.current) {
-                    observer.unobserve(elementRef.current)
-                }
-            }
-        }, [nextShow])
+    const elementRef = useRef(null);
 
     useEffect(() => {
-        const getShows = async() => {
-            try {
-                const response = await axios.get(`${BASE_URL}/shows/get/all`)
-                if (response) {
-                    setShows(response.data)
-                }
-            } catch(error) {
-                console.error('Error fetching shows: ', error)
-            }
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("visible");
+                    } else {
+                        entry.target.classList.remove("visible");
+                    }
+                });
+            },
+            { threshold: 0.1 },
+        );
+
+        if (elementRef.current) {
+            observer.observe(elementRef.current);
         }
-        getShows()
-    }, [])
+
+        return () => {
+            if (elementRef.current) {
+                observer.unobserve(elementRef.current);
+            }
+        };
+    }, [nextShow]);
+
+    useEffect(() => {
+        const getShows = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}shows/get/all`);
+                if (response) {
+                    setShows(response.data);
+                }
+            } catch (error) {
+                console.error("Error fetching shows: ", error);
+            }
+        };
+        getShows();
+    }, []);
 
     useEffect(() => {
         const sortShows = () => {
-            const today = moment().startOf('day');
-        
-            const sortedShowDates = shows
-            .filter(show => moment(show.date).isSameOrAfter(today))
-            .sort((a, b) => moment(a.date).diff(moment(b.date)))
-        
-            setNextShow(sortedShowDates[0]);
-        }
-        sortShows()
-    }, [shows])
+            const today = moment().startOf("day");
 
+            const sortedShowDates = shows
+                .filter((show) => moment(show.date).isSameOrAfter(today))
+                .sort((a, b) => moment(a.date).diff(moment(b.date)));
+
+            setNextShow(sortedShowDates[0]);
+        };
+        sortShows();
+    }, [shows]);
 
     return nextShow ? (
-        <Link to='/shows' className="shows-link" ref={elementRef}>
-            <div className="next-show" >
-                <h1 className='feature-title'>Next Show</h1>
+        <Link to="/shows" className="shows-link" ref={elementRef}>
+            <div className="next-show">
+                <h1 className="feature-title">Next Show</h1>
                 <hr />
-                <h2 className='show-title'>{nextShow.venue}</h2>
+                <h2 className="show-title">{nextShow.venue}</h2>
                 <div className="next-show-container">
-                    <img className='next-show-image' src={nextShow.show_poster} alt="" />
+                    <img
+                        className="next-show-image"
+                        src={nextShow.show_poster}
+                        alt=""
+                    />
                     <div className="info-grid">
                         <h3 className="show-label">Location:</h3>
                         <p className="show-data">{nextShow.location}</p>
                         <h3 className="show-label">Date:</h3>
-                        <p className="show-data">{moment(nextShow.date).format('MMM Do, YYYY')}</p>
+                        <p className="show-data">
+                            {moment(nextShow.date).format("MMM Do, YYYY")}
+                        </p>
                         <h3 className="show-label">Time:</h3>
                         <p className="show-data">{nextShow.time}</p>
                         <h3 className="show-label">Cover?</h3>
@@ -84,5 +91,5 @@ export default function NextShow () {
         </Link>
     ) : (
         <h1>Loading...</h1>
-    )
+    );
 }
