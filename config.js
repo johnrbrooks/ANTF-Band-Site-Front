2ea137092b347079f3caf9e98687b528f9343496
John Brooks <johnrbrooks93@gmail.com>
2026-04-03 09:57:35 -0400

    Moving to a branch, pushing current work

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
-export const VITE_ADMIN_EMAIL = process.env.VITE_ADMIN_EMAIL
-export const VITE_ADMIN_PW = process.env.VITE_ADMIN_PW
+export const VITE_ADMIN_EMAIL = process.env.VITE_ADMIN_EMAIL;
+export const VITE_ADMIN_PW = process.env.VITE_ADMIN_PW;
+export const DEV = process.env.DEV;
+export const BASE_URL =
+    DEV === "true"
+        ? `http://localhost:3001/api/`
+        : `https://antf-band-site-back-production.up.railway.app/api/`;
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import './App.css'
-import Home from './Components/Main/HomePage/Home'
-import About from './Components/Main/About'
-import Shows from './Components/Main/Shows'
-import SongList from './Components/Main/SongList'
-import Contact from './Components/Main/Contact'
-import ScrollToTop from './Components/Utilities/ScrollToTop'
-import AdminLogin from './Components/Admin/AdminLogin/AdminLogin'
-import AdminHome from './Components/Admin/AdminForms/AdminHome'
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Main/HomePage/Home";
+import About from "./Components/Main/About";
+import Shows from "./Components/Main/Shows";
+import SongList from "./Components/Main/SongList";
+import Contact from "./Components/Main/Contact";
+import ScrollToTop from "./Components/Utilities/ScrollToTop";
+import AdminLogin from "./Components/Admin/AdminLogin/AdminLogin";
+import AdminHome from "./Components/Admin/AdminForms/AdminHome";
+// import BASE_URL from "../config.js";
 
-export const BASE_URL = `https://antf-band-site-back-production.up.railway.app/api/`
+export const BASE_URL = `https://antf-band-site-back-production.up.railway.app/api/`;
 
 function App() {
-
-  return (
-    <div className='App'>
-      <ScrollToTop />
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/home" element={<Home />}/>
-        <Route exact path="/about" element={<About />}/>
-        <Route exact path="/shows" element={<Shows />}/>
-        <Route exact path="/songs" element={<SongList />}/>
-        <Route exact path="/contact" element={<Contact />}/>
-        <Route path="/*" element={<h1>404: Page Not Found</h1>}/>
-        <Route exact path="/adminlogin" element={<AdminLogin />}/>
-        <Route exact path="/adminhome" element={<AdminHome />}/>
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="App">
+            <ScrollToTop />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/home" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/shows" element={<Shows />} />
+                <Route exact path="/songs" element={<SongList />} />
+                <Route exact path="/contact" element={<Contact />} />
+                <Route path="/*" element={<h1>404: Page Not Found</h1>} />
+                <Route exact path="/adminlogin" element={<AdminLogin />} />
+                <Route exact path="/adminhome" element={<AdminHome />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
